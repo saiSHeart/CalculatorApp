1e760a206d81ce24bce8b0d0fffee5e9d8e73369
Sai Krishna <sai@surveyheart.com>
2022-03-16 19:25:00 +0530

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by sai krishna korukanti on 16/03/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
